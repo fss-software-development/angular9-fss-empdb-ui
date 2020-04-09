43ac7e0f436944fa263200d3b8896ec37afda3ac
Monika-G <62936041+Monika-G@users.noreply.github.com>
2020-04-09 19:17:17 +0530

    routing changes
    I just did some changes for local routing to route the page from one page to another locally and added routing for menu page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { UpdateEmployeeComponent } from './update-employee/update-employee.compo
 import { LoginComponent } from './login/login.component';
 import { AuthGuardService } from './auth-guard.service';
 import { LogoutComponent } from './logout/logout.component';
+import{ MenuPageComponent }from './menu-page/menu-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'employee', pathMatch: 'full' ,canActivate:[AuthGuardService]},
@@ -16,6 +17,7 @@ const routes: Routes = [
   { path: 'add', component: CreateEmployeeComponent,canActivate:[AuthGuardService] },
   { path: 'update/:id', component: UpdateEmployeeComponent,canActivate:[AuthGuardService] },
   { path: 'details/:id', component: EmployeeDetailsComponent,canActivate:[AuthGuardService] },
+  {path: 'menu-page', component: MenuPageComponent,canActivate:[AuthGuardService]}
 ];
 
 @NgModule({
@@ -23,3 +25,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingcomponent= [LoginComponent, LogoutComponent ,EmployeeListComponent ,CreateEmployeeComponent,
+  UpdateEmployeeComponent,EmployeeDetailsComponent,MenuPageComponent]
